0df79885110c6d0214bac634efefcd6cb2ce66d7
daniel azara <daniel.azara@pdcase.com.br>
2025-07-31 19:12:57 -0300

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
-import LoginPage from './pages/Auth/Login/LoginPage'
-import SignupPage from './pages/Auth/Signup/SignupPage'
-import ForgotPage from './pages/Auth/ForgotPassword/ForgotPassword'
+import AppRoutes from './routes'
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/forgot-password" element={<ForgotPage/>}/>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-        </Routes> 
-      </div>
-    </Router>
-  )
+  return <AppRoutes/>
 }
 
 export default App
---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/Auth/Login/LoginPage";
+import SignupPage from "./pages/Auth/Signup/SignupPage";
+import ForgotPage from "./pages/Auth/ForgotPassword/ForgotPassword";
+
+export default function AppRoutes() {
+    return (
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/forgot-password" element={<ForgotPage/>}/>
+          <Route path="/home" element={<Home/>} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+       </Routes>     
+    )
+}
